2c021420a805e3d138599414bed8c10ed0afe3c3
tjrkdgnl <skh0818@hanyang.ac.kr>
2021-11-07 13:19:13 +0900

    modified : 코루틴 데이터 수정

--- app/src/main/java/com/ama/algorithmmanagement/Activity/KCallAPIActivity.kt
+++ app/src/main/java/com/ama/algorithmmanagement/Activity/KCallAPIActivity.kt
@@ -7,6 +7,9 @@ import com.ama.algorithmmanagement.Base.KBaseActivity
 import com.ama.algorithmmanagement.R
 import com.ama.algorithmmanagement.databinding.DefaultActivityCallApiBinding
 import com.ama.algorithmmanagement.viewmodel.KAPICallViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class KCallAPIActivity :
     KBaseActivity<DefaultActivityCallApiBinding>(R.layout.default_activity_call_api) {
@@ -18,6 +21,10 @@ class KCallAPIActivity :
         callViewModel = ViewModelProvider(this).get(KAPICallViewModel::class.java)
         binding.viewModel = callViewModel
         binding.recyclerviewCallApi.adapter = KDefaultRecyclerViewAdapter()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val data ="홍철이바보"
+        }
 
     }
 }
