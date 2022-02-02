d503b555caaaf9a343a797e11dd5c507055bc9e0
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-02 17:43:35 +0900

    feat : 나의 실력그래프 날짜별 통계부분 캘린더 관련 유틸 함수

--- app/src/main/java/com/ama/algorithmmanagement/utils/DateUtils.kt
+++ app/src/main/java/com/ama/algorithmmanagement/utils/DateUtils.kt
@@ -17,4 +17,23 @@ object DateUtils {
         return date
     }
 
+    private val statsDate by lazy{
+        val dateFormat = SimpleDateFormat("yyyy.MM.dd")
+        val currentDate = dateFormat.parse(date)
+        val calendar:Calendar = Calendar.getInstance()
+        calendar.time = currentDate
+        calendar
+    }
+    fun nextMonth(){
+        statsDate.set(Calendar.MONTH, statsDate.get(Calendar.MONTH)+1)
+    }
+    fun prevMonth(){
+        statsDate.set(Calendar.MONTH, statsDate.get(Calendar.MONTH)-1)
+    }
+
+    fun getCalendarDate():String = "${statsDate.get(Calendar.YEAR)}.${statsDate.get(Calendar.MONTH)+1}.${statsDate.get(Calendar.DAY_OF_MONTH)}"
+
+    fun getCalendarYearMonth():String = "${statsDate.get(Calendar.YEAR)}년 ${statsDate.get(Calendar.MONTH)+1}월"
+
+
 }
