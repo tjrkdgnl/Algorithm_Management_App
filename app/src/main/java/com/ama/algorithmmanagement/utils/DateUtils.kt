ff752b1f52d0742a7515433887deac0772a95344
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-06 23:21:59 +0900

    feat : Calendar 로 날짜 관련 함수 작성

--- app/src/main/java/com/ama/algorithmmanagement/utils/DateUtils.kt
+++ app/src/main/java/com/ama/algorithmmanagement/utils/DateUtils.kt
@@ -1,8 +1,10 @@
 package com.ama.algorithmmanagement.utils
 
 import android.annotation.SuppressLint
+import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.min
 
 @SuppressLint("SimpleDateFormat")
 object DateUtils {
@@ -38,6 +40,21 @@ object DateUtils {
     fun getCalendarDate():String = "${statsDate.get(Calendar.YEAR)}.${statsDate.get(Calendar.MONTH)+1}.${statsDate.get(Calendar.DAY_OF_MONTH)}"
 
     fun getCalendarYearMonth():String = "${statsDate.get(Calendar.YEAR)}년 ${statsDate.get(Calendar.MONTH)+1}월"
+
+    fun getStatDate() = statsDate
+
+    // 현재 월의 첫 시작일(n월 1일)의 위치(요일) 리턴
+    fun getStatDateStartDay():Int{
+        val tempDay = statsDate.get(Calendar.DAY_OF_MONTH)
+        statsDate.set(Calendar.DAY_OF_MONTH,1)
+        val startDay = statsDate.get(Calendar.DAY_OF_WEEK)
+        statsDate.set(Calendar.DAY_OF_MONTH,tempDay)
+        return startDay
+    }
+    // 현재 월의 최대 일(n월의 끝일 ex) 3월의 경우 31일이 끝일이기때문에 31 리턴)
+    fun getStatDateMaxDay():Int = statsDate.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+
 
     fun getYear(): Int {
         return dateArray[0]
