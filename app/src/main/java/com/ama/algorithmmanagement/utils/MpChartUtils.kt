af58a0bc441c4b8d83747039060c345988a14161
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-06 23:21:23 +0900

    feat : 유형별 통계에서 유형별로 몇문제를 풀었는지 해시맵으로 만들어주는 함수 추가

--- app/src/main/java/com/ama/algorithmmanagement/utils/MpChartUtils.kt
+++ app/src/main/java/com/ama/algorithmmanagement/utils/MpChartUtils.kt
@@ -6,7 +6,9 @@ import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.data.BarEntry
 
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.StackedValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
+import timber.log.Timber
 
 
 /**
@@ -18,6 +20,7 @@ import com.github.mikephil.charting.formatter.ValueFormatter
  * description : mpChart 라이브러리 관련 함수 및 클래스 정의
  */
 object MpChartUtils {
+    // x 축 값에서 레이블 지정( horizontal bar 의 경우 y 축)
     class LabelCustomFormatter(private val labels:List<String>) : ValueFormatter() {
         private var index = 0
 
@@ -31,24 +34,53 @@ object MpChartUtils {
         }
     }
 
+    // bar 마다 값을 표시해줌
     class ScoreCustomFormatter : ValueFormatter() {
         override fun getFormattedValue(value: Float): String {
+            // float 형태이기 떄문에 소수점 짜르고 정수부분만 출력
             val score = value.toString().split(".")
-            return score[0]
+            return "${score[0]} 점"
         }
     }
-    fun getTypeStats(problems: Problems?):HashMap<String,Int>?{
+
+    // 유형별로 저장된 해시맵에서 몇개씩 뿌려줄건지 지정
+    fun getTypeStatsSize(problemList:MutableList<TaggedProblem>,size:Int=10,isAll:Boolean=true):HashMap<String,Int>{
+        val getHashMap = getTypeStats(problemList)
+        Timber.e("get hashmap $getHashMap")
+        if(isAll){
+            Timber.e("????")
+            return getHashMap
+        }
+        val sizeHashMap = HashMap<String,Int>()
+        val typeList = getHashMap.keys.toList()
+        if(getHashMap.isNotEmpty()){
+            for(i in 0..size){
+                getHashMap[typeList[i]]?.let {
+                    sizeHashMap.put(typeList[i], it)
+                }
+            }
+        }
+        Timber.e("sizehashmap $sizeHashMap")
+        return sizeHashMap
+
+    }
+
+    // 유형별로 몇개 풀었는지 해시맵에 저장
+    private fun getTypeStats(problemList: MutableList<TaggedProblem>?):HashMap<String,Int>{
         val hashMap = HashMap<String,Int>()
-        val problemList = problems?.problemList
         problemList?.map {  taggedProblem ->
             taggedProblem.tags.map { Tag->
                 Tag.displayNames.map { displayName ->
-                    if(hashMap.containsKey(displayName.name)){
-                        hashMap[displayName.name]?.let{
-                            hashMap[displayName.name] = it+1
+                    // 태그가 한국어인경우만 통계 냄
+                    if(displayName.language=="ko"){
+                        if(hashMap.containsKey(displayName.name)){
+                            // 해시맵에 키가 존재할경우 1씩 증가시키고 아닐경우 1로 세팅
+                            hashMap[displayName.name]?.let{
+                                hashMap[displayName.name] = it+1
+                            }
+                        }else{
+                            hashMap[displayName.name] = 1
                         }
-                    }else{
-                        hashMap[displayName.name] = 1
                     }
                 }
             }
