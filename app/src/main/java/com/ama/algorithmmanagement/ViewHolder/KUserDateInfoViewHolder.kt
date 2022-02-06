2f574a47f6c1d2ac37ab0a8b60a8cdd4560b1000
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-06 23:26:14 +0900

    feat : dateInfo 객체에 담긴 count 값에 따라 알맞는 색상을 보여주게 세팅

--- app/src/main/java/com/ama/algorithmmanagement/ViewHolder/KUserDateInfoViewHolder.kt
+++ app/src/main/java/com/ama/algorithmmanagement/ViewHolder/KUserDateInfoViewHolder.kt
@@ -1,10 +1,16 @@
 package com.ama.algorithmmanagement.ViewHolder;
 
+import android.graphics.Color
 import android.view.ViewGroup
 import com.ama.algorithmmanagement.Base.KBaseViewHolder
 import com.ama.algorithmmanagement.R
 import com.ama.algorithmmanagement.databinding.ItemUserDateInfoBinding
 import com.ama.algorithmmanagement.model.DateInfo
+import com.ama.algorithmmanagement.model.DateObject
+import com.ama.algorithmmanagement.model.YearInfo
+import com.ama.algorithmmanagement.utils.ColorUtils
+import com.ama.algorithmmanagement.utils.DateUtils
+import timber.log.Timber
 
 /**
  * @author : seungHo
@@ -18,7 +24,8 @@ class KUserDateInfoViewHolder(private val parent: ViewGroup) :
     KBaseViewHolder<ItemUserDateInfoBinding>(
         parent, R.layout.item_user_date_info
     ) {
-        fun setData(dateInfo: DateInfo){
-
-        }
+    fun setData(dateInfo: DateInfo) {
+        val dateValue = ColorUtils.dateInfoConvert(dateInfo.count)
+        binding.content.setBackgroundColor(dateValue)
+    }
 }
