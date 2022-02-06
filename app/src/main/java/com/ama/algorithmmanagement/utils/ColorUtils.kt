07e06bd598f2d6770b4a7bd6560f46d7ad64b497
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-06 23:22:55 +0900

    feat : 날짜별 통계에서 사용될 색상 관련 함수 및 변수 정의

--- app/src/main/java/com/ama/algorithmmanagement/utils/ColorUtils.kt
+++ app/src/main/java/com/ama/algorithmmanagement/utils/ColorUtils.kt
@@ -12,56 +12,83 @@ import com.ama.algorithmmanagement.R
  * class : TierColors.java
  * github : devaspirant0510
  * email : seungho020510@gmail.com
- * description : 티어별 색상 코드리스트
+ * description : 색상관련 유틸함수
  */
-object TierColors {
-    // enum 으로 변경
+object ColorUtils {
+    // 나의 실력그래프 티어별 색상 ENUM
     enum class TierTypes{
         BRONZE,SILVER,GOLD,PLATINUM,DIAMOND,RUBY
     }
+    // 나의 실력 그래프 날짜별 통계 색상 ENUM
+    enum class DateInfoTypes{
+        ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NO
+    }
 
-    const val BRONZE = 0
-    const val SILVER = 1
-    const val GOLD = 2
-    const val PLATINUM = 3
-    const val DIALMOND = 4
-    const val RUBY = 5
-    val bronzeList = mutableListOf<Int>(
+    // 날짜별 통계에 사용될 색상 리스트로 묶음
+    val dateInfoColors = listOf<Int>(
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_0),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_1),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_2),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_3),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_4),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_6),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_7),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_8),
+        ContextCompat.getColor(AMAApplication.INSTANCE,R.color.date_blank),
+    )
+/*    fun getDateInfoType(value:Int):DateInfoTypes{
+        return when(value){
+            -1->DateInfoTypes.NO
+            0->DateInfoTypes.ZERO
+            1->DateInfoTypes.ONE
+            2->DateInfoTypes.TWO
+            3->DateInfoTypes.THREE
+            4->DateInfoTypes.FOUR
+            5->DateInfoTypes.FIVE
+            6->DateInfoTypes.SIX
+            7->DateInfoTypes.SEVEN
+            8->DateInfoTypes.EIGHT
+            else->DateInfoTypes.NO
+        }
+    }*/
+
+
+    private val bronzeList = listOf(
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.bronze_5),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.bronze_4),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.bronze_3),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.bronze_2),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.bronze_1),
     )
-    val silverList = mutableListOf<Int>(
+    private val silverList = listOf(
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.silver_5),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.silver_4),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.silver_3),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.silver_2),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.silver_1),
     )
-    val goldList = mutableListOf<Int>(
+    private val goldList = listOf(
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.gold_5),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.gold_4),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.gold_3),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.gold_2),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.gold_1),
     )
-    val platinumList = mutableListOf<Int>(
+    private val platinumList = listOf(
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.platinum_5),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.platinum_4),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.platinum_3),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.platinum_2),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.platinum_1),
     )
-    val diamondList = mutableListOf<Int>(
+    private val diamondList = listOf(
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.diamond_5),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.diamond_4),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.diamond_3),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.diamond_2),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.diamond_1),
     )
-    val rubyList = mutableListOf<Int>(
+    private val rubyList = listOf(
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.ruby_5),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.ruby_4),
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.ruby_3),
@@ -69,27 +96,58 @@ object TierColors {
         ContextCompat.getColor(AMAApplication.INSTANCE, R.color.ruby_1),
     )
 
-    fun getTierName(tier: Int): String {
+    // 해당 티어의 Enum 을 넘기면 넘긴 정보를 문자로 리턴
+    fun getTierName(tier: TierTypes): String {
         return when (tier) {
-            BRONZE -> "bronze"
-            SILVER -> "silver"
-            GOLD -> "gold"
-            PLATINUM -> "platinum"
-            DIALMOND -> "diamond"
-            RUBY -> "ruby"
+            TierTypes.BRONZE -> "bronze"
+            TierTypes.SILVER -> "silver"
+            TierTypes.GOLD -> "gold"
+            TierTypes.PLATINUM -> "platinum"
+            TierTypes.DIAMOND -> "diamond"
+            TierTypes.RUBY -> "ruby"
             else -> "none"
         }
     }
-
-    fun getTierList(tier: Int): MutableList<Int>? {
-        return when (tier) {
-            BRONZE -> bronzeList
-            SILVER -> silverList
-            GOLD -> goldList
-            PLATINUM -> platinumList
-            DIALMOND -> diamondList
-            RUBY -> rubyList
-            else -> null
+    // 정수를 넘기면 Enum 으로 리턴
+    fun tierConvertInt(pos:Int):TierTypes{
+        return when(pos){
+            0->TierTypes.BRONZE
+            1->TierTypes.SILVER
+            2->TierTypes.GOLD
+            3->TierTypes.PLATINUM
+            4->TierTypes.DIAMOND
+            5->TierTypes.RUBY
+            else->TierTypes.BRONZE
         }
+
+    }
+    // 티어 enum 을 넘기면 해당 티어의 색상코드 리스트를 넘김
+    fun getTierList(tier: TierTypes): List<Int> {
+        return when (tier) {
+            TierTypes.BRONZE -> bronzeList
+            TierTypes.SILVER -> silverList
+            TierTypes.GOLD -> goldList
+            TierTypes.PLATINUM -> platinumList
+            TierTypes.DIAMOND -> diamondList
+            TierTypes.RUBY -> rubyList
+            else -> bronzeList
+        }
+    }
+
+    // 정수를 넘기면 색상값으로 리턴
+    fun dateInfoConvert(count:Int):Int{
+        return when(count){
+            -1->dateInfoColors[8]
+            0->dateInfoColors[0]
+            1->dateInfoColors[1]
+            2->dateInfoColors[2]
+            3->dateInfoColors[3]
+            4->dateInfoColors[4]
+            5->dateInfoColors[5]
+            6->dateInfoColors[6]
+            7->dateInfoColors[7]
+            else -> dateInfoColors[7]
+        }
+
     }
 }
