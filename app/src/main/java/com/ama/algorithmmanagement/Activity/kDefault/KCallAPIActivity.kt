7d5c81138a736821df83782bc3afe651b4ced861
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-08 14:48:19 +0900

    modify : 추가 안된 파일 추가

---
+++ app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/KCallAPIActivity.kt
@@ -0,0 +1,33 @@
+package com.ama.algorithmmanagement.Activity.kDefault
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.ama.algorithmmanagement.Adapter.KDefaultRecyclerViewAdapter
+import com.ama.algorithmmanagement.Application.AMAApplication
+import com.ama.algorithmmanagement.Base.BaseViewModelFactory
+import com.ama.algorithmmanagement.Base.KBaseActivity
+import com.ama.algorithmmanagement.R
+import com.ama.algorithmmanagement.Repositories.RepositoryLocator
+import com.ama.algorithmmanagement.databinding.DefaultActivityCallApiBinding
+import com.ama.algorithmmanagement.viewmodel.kDefault.KAPICallViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+class KCallAPIActivity :
+    KBaseActivity<DefaultActivityCallApiBinding>(R.layout.default_activity_call_api) {
+
+    private lateinit var callViewModel: KAPICallViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        callViewModel = ViewModelProvider(
+            this,
+            BaseViewModelFactory(RepositoryLocator().getFakeRepository(AMAApplication.INSTANCE))
+        )[KAPICallViewModel::class.java]
+
+        binding.viewModel = callViewModel
+        binding.recyclerviewCallApi.adapter = KDefaultRecyclerViewAdapter()
+
+
+    }
+}
