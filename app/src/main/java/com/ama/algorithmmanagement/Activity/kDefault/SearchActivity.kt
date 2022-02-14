811adb7803e20d95fe530daed91bbfabb38f8990
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-14 17:51:53 +0900

    feat : editText 에서 양방향 데이터바인딩한 라이브데이터 observe  하여서 뷰모델에 검색결과 세팅하는 함수 호출

--- app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/SearchActivity.kt
+++ app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/SearchActivity.kt
@@ -2,6 +2,7 @@ package com.ama.algorithmmanagement.Activity.kDefault;
 
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import com.ama.algorithmmanagement.Adapter.SearchProblemAdapter
 import com.ama.algorithmmanagement.Application.AMAApplication
 import com.ama.algorithmmanagement.Base.BaseViewModelFactory
 import com.ama.algorithmmanagement.Base.KBaseActivity
@@ -9,6 +10,7 @@ import com.ama.algorithmmanagement.R
 import com.ama.algorithmmanagement.Repositories.RepositoryLocator
 import com.ama.algorithmmanagement.databinding.ActivitySearchBinding
 import com.ama.algorithmmanagement.viewmodel.kDefault.SearchViewModel
+import timber.log.Timber
 
 /**
  * @author : seungHo
@@ -16,7 +18,7 @@ import com.ama.algorithmmanagement.viewmodel.kDefault.SearchViewModel
  * class : KSearchActivity.java
  * github : devaspirant0510
  * email : seungho020510@gmail.com
- * description :
+ * description : SearchActivity 검색 API 를 이용한 검색화면
  */
 class SearchActivity : KBaseActivity<ActivitySearchBinding>(R.layout.activity_search) {
     private lateinit var viewModel: SearchViewModel
@@ -27,6 +29,19 @@ class SearchActivity : KBaseActivity<ActivitySearchBinding>(R.layout.activity_se
             BaseViewModelFactory(RepositoryLocator().getRepository(AMAApplication.INSTANCE))
         )[SearchViewModel::class.java]
         binding.viewmodel = viewModel
+        val searchAdapter = SearchProblemAdapter()
+        binding.rvSearchProblem.adapter = searchAdapter
+        // editText 의 상태 즉 텍스트를 입력할때마다 네트워크 요청
+        viewModel.inputSearch.observe(this,{
+            // solved api 에서 "" 로 요청했을때 데이터가 던져줘서 막음
+            val str = it.replace(" ","") // 데이터 없이 빈칸 "   " 이런식으로 보내도 기본 데이터 던져줘서 막음
+            Timber.e("str: $str")
+            if(str.isNotEmpty()){
+                viewModel.callSearchQueryProblem(it)
+            }else{
+                searchAdapter.cleanData()
+            }
+        })
 
     }
 }
