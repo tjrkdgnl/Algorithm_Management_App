b278dcdf4110d9118f7eede812e84090b7cd353d
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-14 17:43:12 +0900

    feat : 검색 API 호출 뷰모델 연결

--- app/src/main/java/com/ama/algorithmmanagement/viewmodel/kDefault/SearchViewModel.kt
+++ app/src/main/java/com/ama/algorithmmanagement/viewmodel/kDefault/SearchViewModel.kt
@@ -3,8 +3,12 @@ package com.ama.algorithmmanagement.viewmodel.kDefault;
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ama.algorithmmanagement.Base.BaseRepository
+import com.ama.algorithmmanagement.model.AutoKeywordObject
 import com.ama.algorithmmanagement.model.Problems
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 /**
  * @author : seungHo
@@ -12,17 +16,26 @@ import com.ama.algorithmmanagement.model.Problems
  * class : SearchViewModel.java
  * github : devaspirant0510
  * email : seungho020510@gmail.com
- * description :
+ * description : SearchActivity.kt 뷰모델
  */
 class SearchViewModel(private val mRepository: BaseRepository):ViewModel() {
+    // editText 양방향 바인딩
     val inputSearch = MutableLiveData<String>()
 
-    val searchProblems = MutableLiveData<Problems>()
-    private val _searchProblems:LiveData<Problems>
-        get() = searchProblems
+    // 검색결과 라이브데이터
+    private val _searchProblems = MutableLiveData<AutoKeywordObject>()
+    val searchProblems:LiveData<AutoKeywordObject>
+        get() = _searchProblems
 
-    // 검색 자동완성
+    // 검색 자동완성 API 호출
     fun callSearchQueryProblem(query:String){
+        viewModelScope.launch {
+            try{
+                _searchProblems.value = mRepository.getAutoSearchedData(query)
+            }catch (e:Exception){
+                Timber.e("error : $e")
+            }
+        }
 
     }
 
