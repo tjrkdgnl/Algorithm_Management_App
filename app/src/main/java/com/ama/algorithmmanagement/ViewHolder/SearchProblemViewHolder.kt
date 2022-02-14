8e6b35fb2b7944a1846fb769692dd0e05f2fa6d3
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-14 17:29:08 +0900

    add : 검색화면 어댑터 뷰홀더

---
+++ app/src/main/java/com/ama/algorithmmanagement/ViewHolder/SearchProblemViewHolder.kt
@@ -0,0 +1,23 @@
+package com.ama.algorithmmanagement.ViewHolder;
+
+import android.view.ViewGroup
+import com.ama.algorithmmanagement.Base.KBaseViewHolder
+import com.ama.algorithmmanagement.R
+import com.ama.algorithmmanagement.databinding.ItemSearchProblemBinding
+import com.ama.algorithmmanagement.model.Keyword
+
+/**
+ * @author : seungHo
+ * @since : 2022-02-14
+ * class : SearchProblemViewHolder.java
+ * github : devaspirant0510
+ * email : seungho020510@gmail.com
+ * description : 검색 결과 리사이클러뷰 뷰홀더
+ */
+class SearchProblemViewHolder(parent:ViewGroup) :KBaseViewHolder<ItemSearchProblemBinding>(parent,
+    R.layout.item_search_problem){
+    // TODO : 파이어베이스에서 해당 문제를 풀었다면 체크표시
+    fun setData(data:Keyword){
+        binding.keyword = data
+    }
+}
