9b466983af653f038439cbfa090e76342c7382dc
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-15 01:09:16 +0900

    add : webview 액티비티 세팅

---
+++ app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/WebViewActivity.kt
@@ -0,0 +1,21 @@
+package com.ama.algorithmmanagement.Activity.kDefault;
+
+import android.os.Bundle
+import com.ama.algorithmmanagement.Base.KBaseActivity
+import com.ama.algorithmmanagement.R
+import com.ama.algorithmmanagement.databinding.ActivityWebViewBinding
+
+/**
+ * @author : seungHo
+ * @since : 2022-02-14
+ * class : WebViewActivity.java
+ * github : devaspirant0510
+ * email : seungho020510@gmail.com
+ * description :
+ */
+class WebViewActivity :KBaseActivity<ActivityWebViewBinding>(R.layout.activity_web_view){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
