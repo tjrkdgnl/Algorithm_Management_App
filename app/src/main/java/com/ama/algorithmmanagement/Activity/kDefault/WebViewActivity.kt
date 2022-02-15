b6f99dc68ce7bde0f63cf6515d48209fc3506565
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-15 21:15:27 +0900

    feat : intent로 문제 아이디 받아 해당 문제의 링크를 웹뷰로 보여줌

--- app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/WebViewActivity.kt
+++ app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/WebViewActivity.kt
@@ -1,9 +1,11 @@
-package com.ama.algorithmmanagement.Activity.kDefault;
+package com.ama.algorithmmanagement.Activity.kDefault
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelStore
 import com.ama.algorithmmanagement.Base.KBaseActivity
 import com.ama.algorithmmanagement.R
 import com.ama.algorithmmanagement.databinding.ActivityWebViewBinding
+import com.ama.algorithmmanagement.viewmodel.kDefault.NewSolvedProblemViewModel
 
 /**
  * @author : seungHo
@@ -11,11 +13,16 @@ import com.ama.algorithmmanagement.databinding.ActivityWebViewBinding
  * class : WebViewActivity.java
  * github : devaspirant0510
  * email : seungho020510@gmail.com
- * description :
+ * description : 웹뷰 액티비티
  */
-class WebViewActivity :KBaseActivity<ActivityWebViewBinding>(R.layout.activity_web_view){
+class WebViewActivity : KBaseActivity<ActivityWebViewBinding>(R.layout.activity_web_view) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        // intent 에서 전달받은값 받아서와서 url 을 뿌려줌
+        val getUrl = intent.getIntExtra("problemId", 1000)
+        binding.webview.loadUrl("https://www.acmicpc.net/problem/$getUrl")
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
