f36b9598c64d97b199919c26521dfce1d1c5f83e
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-15 16:40:22 +0900

    modfiy : Timer 대신 코루틴 Job 을 이용해 검색 호출 API 구현

--- app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/SearchActivity.kt
+++ app/src/main/java/com/ama/algorithmmanagement/Activity/kDefault/SearchActivity.kt
@@ -3,6 +3,7 @@ package com.ama.algorithmmanagement.Activity.kDefault;
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import com.ama.algorithmmanagement.Adapter.SearchProblemAdapter
 import com.ama.algorithmmanagement.Application.AMAApplication
@@ -35,10 +36,11 @@ class SearchActivity : KBaseActivity<ActivitySearchBinding>(R.layout.activity_se
         val searchAdapter = SearchProblemAdapter()
         binding.rvSearchProblem.adapter = searchAdapter
 
-        // 참고 : https://stackoverflow.com/questions/12142021/how-can-i-do-something-0-5-seconds-after-text-changed-in-my-edittext-control
-        binding.etInputSearch.addTextChangedListener(object: TextWatcher {
-            // 타이머 객체생성
-            var timer = Timer()
+        binding.etInputSearch.onChangeDebounceOption()
+    }
+
+    private fun EditText.onChangeDebounceOption() {
+        this.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 Timber.e("beforeTextChanged $p0")
             }
@@ -48,20 +50,12 @@ class SearchActivity : KBaseActivity<ActivitySearchBinding>(R.layout.activity_se
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                timer.cancel()// 취소 (예약된 작업도 취소함)
-                timer = Timer() // 타이머객체 다시만들고
                 // 공백이 들어간상태에선 요청 막음
-                if(p0.toString().isNotEmpty() && p0.toString().isNotBlank()){
-                    timer.schedule(object:TimerTask(){
-                        override fun run() {
-                            viewModel.callSearchQueryProblem(p0.toString())
-                        }
-
-                    },600) // 0.6초 뒤에 예약할작업 지정
+                if (p0.toString().isNotEmpty() && p0.toString().isNotBlank()) {
+                    viewModel.callSearchQueryProblem(p0.toString())
                 }
-                // 즉 schedule 에서 0.6 초 대기시키고 run 안에있는 함수를 실행시키는데
-                // afterTextChanged 첫줄에 cancel- 함수가 있어 0.6 초 이내에 값이 바뀔경우 예약된 작업도 취소됨
             }
+
         })
     }
 }
