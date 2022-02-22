507d443d5205427b030dc49f8e17ad85bdfce525
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-02-23 00:49:22 +0900

    modify : 해당 함수에대한 주석 추가

--- app/src/main/java/com/ama/algorithmmanagement/utils/TipUtils.kt
+++ app/src/main/java/com/ama/algorithmmanagement/utils/TipUtils.kt
@@ -3,9 +3,15 @@ package com.ama.algorithmmanagement.utils
 import com.ama.algorithmmanagement.model.Tag
 
 object TipUtils {
-    fun tagsConvertToString(tags:List<Tag>):String{
+    /**
+     * @param tags Problem 에서 태그리스트
+     * @return 태그리스트에서 한국어로된 태그만 문자열로 리턴
+     * 팁 작성화면에서 문제정보를 가져올때 List< Tags > 에서
+     * 한국어로 된 유형만 가져와서 문자열로 뿌려줌
+     */
+    fun tagsConvertToString(tags:List<Tag>?):String{
         var result = ""
-        tags.map{tag->
+        tags?.map{tag->
             tag.displayNames.map{ dp->
                 if(dp.language=="ko"){
                     result+="${dp.name},"
