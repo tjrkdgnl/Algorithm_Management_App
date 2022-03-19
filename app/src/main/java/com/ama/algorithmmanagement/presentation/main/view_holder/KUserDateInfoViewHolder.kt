cbc734efc5fe585f93b7efb105fa5277a8997fc6
Seung <seungho020510@gmail.com>
2022-03-19 23:51:45 +0900

    modify : drawble 적용, 날짜 보여주는 방식 변경

--- app/src/main/java/com/ama/algorithmmanagement/presentation/main/view_holder/KUserDateInfoViewHolder.kt
+++ app/src/main/java/com/ama/algorithmmanagement/presentation/main/view_holder/KUserDateInfoViewHolder.kt
@@ -1,11 +1,14 @@
 package com.ama.algorithmmanagement.presentation.main.view_holder;
 
+import android.content.res.ColorStateList
 import android.view.ViewGroup
+import androidx.core.content.res.ColorStateListInflaterCompat
 import com.ama.algorithmmanagement.domain.base.KBaseViewHolder
 import com.ama.algorithmmanagement.R
 import com.ama.algorithmmanagement.databinding.ItemUserDateInfoBinding
 import com.ama.algorithmmanagement.data.model.DateInfo
 import com.ama.algorithmmanagement.utils.ColorUtils
+import timber.log.Timber
 
 /**
  * @author : seungHo
@@ -19,8 +22,17 @@ class KUserDateInfoViewHolder(private val parent: ViewGroup) :
     KBaseViewHolder<ItemUserDateInfoBinding>(
         parent, R.layout.item_user_date_info
     ) {
-    fun setData(dateInfo: DateInfo) {
-        val dateValue = ColorUtils.dateInfoConvert(dateInfo.count)
-        binding.content.setBackgroundColor(dateValue)
+    fun setData(dateTriple: Triple<DateInfo,Int,Boolean>) {
+        binding.apply {
+            Timber.e("$dateTriple")
+            // 날짜표시
+            date = dateTriple.second.toString()
+            // 현재달의 날짜인지 여부 , 이전달의 날짜 다음달의 날짜의경우 글자 흐려짐
+            isCurrentDate = dateTriple.third
+            // 유저의 활동개수에 따라 색상 다르게
+            content.backgroundTintList = ColorStateList.valueOf(ColorUtils.dateInfoConvert(dateTriple.first.count))
+        }
+       // binding.content.backgroundTintList = ColorStateList.valueOf(parent.context.resources.getColor(R.color.barchart_color))
+//        binding.content.setBackgroundColor(dateValue)
     }
 }
